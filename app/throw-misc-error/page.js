d4ef6d7e0e8e9b6fe00dc4485a6792b09c7dc752
pauldeason <deason_paul@hotmail.com>
2025-07-25 10:41:22 +0100

    Demo

--- app/throw-misc-error/page.js
+++ app/throw-misc-error/page.js
@@ -2,11 +2,11 @@ import {headers} from "next/headers";
 
 export async function generateMetadata({params}) {
     const {preventStaticRender} = await headers();
+    await remoteDataFetch();
     throw new Error("This is an error");
 }
 
 export default async function ThrowMiscErrorPage({params, searchParams}) {
-    await remoteDataFetch();
     return (
         <>This page throws a misc error</>
     )
